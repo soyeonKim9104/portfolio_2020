a02df8c071ff25daaade6a6310d492f93da85e62
soyeonKim9104 <soyean7979@gmail.com>
2020-01-03 17:54:13 +0900

    2020-01-03

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+function menuToggle() {
+    var nav = document.getElementById('menu-overlay');
+    nav.classList.toggle('active');
+
+    var nav = document.getElementById('toggleIcon');
+    nav.classList.toggle('active');
+}
